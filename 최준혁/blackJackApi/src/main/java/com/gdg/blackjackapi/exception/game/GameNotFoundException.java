b8af7ac4946c081c00fe010bae2af1188f069bdd
nmmn1004 <jjj4120@gmail.com>
2025-11-10 18:13:29 +0900

    Style: Delete the blank in Exception of GameNotFoundException

--- 최준혁/blackJackApi/src/main/java/com/gdg/blackjackapi/exception/game/GameNotFoundException.java
+++ 최준혁/blackJackApi/src/main/java/com/gdg/blackjackapi/exception/game/GameNotFoundException.java
@@ -1,6 +1,6 @@
 package com.gdg.blackjackapi.exception.game;
 
-public class GameNotFoundException extends  RuntimeException{
+public class GameNotFoundException extends RuntimeException{
     public GameNotFoundException(Long id) {
         super("ID(" + id + ")는 존재하지 않는 게임입니다.");
     }
